599250d482f193a32379686b40dc25cda534c817
heatblazer <ilianzapryanov@abv.bg>
2017-06-14 17:05:56 +0300

    fixed meshlab is now working

--- widgettest.py
+++ widgettest.py
@@ -1,7 +1,14 @@
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QTimer, QObject
+import sys
 import test
 
+
 def main():
+    app = QApplication(sys.argv)
     t = test.Test()
+    t.initMeshlab()
+    return  app.exec_()
 
 if __name__ == "__main__":
     main()
